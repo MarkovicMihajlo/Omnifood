32b9a4c64909135152c5789c43c8121b7769f459
MarkovicMihajlo <mihajlomarkovic003@gmail.com>
2025-07-21 21:07:02 +0200

    making mobile navigation work

--- js/script.js
+++ js/script.js
@@ -2,3 +2,11 @@
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
+
+//Make mobile navigation work
+const btnNav = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNav.addEventListener('click', function(){
+    headerEl.classList.toggle('nav-open')
+});
